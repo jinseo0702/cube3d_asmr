4d11dc387d1ebd5ab709cab6e861a8d4a8d9fa44
Hyunjun Ahn <hyunahn@c2r2s4.42gyeongsan.kr>
2024-12-26 19:58:01 +0900

    red two

--- cub3d.h
+++ cub3d.h
@@ -10,19 +10,22 @@
 
 typedef struct s_img
 {
-	void					*img;
-	int						width;
-	int						height;
-    int						endian;
+	void					*img;   //이미지
+    char					*buffer;    //mlx_get_data_addr 이 함수 쓸때 저장할거
+	int						width;      //이미지의 길이
+	int						height;     //이미지의 높이
+    int						pixel_bits; //mlx_new_image 이 함수 쓸때, 사용할 픽셀 비트
+	int						line_bytes; //mlx_new_image 이 함수 쓸 때, 사용한 라인의 바이트
+    int						endian;     //mlx_new_image 이 함수 쓸 때, 사용할 엔디안 값
 }			t_img;
 
 typedef struct  s_data 
 {
-    void    *mlx;
-    void    *win;
-    int						width;
-	int						height;
-    t_img   img;
+    void    *mlx;       //mlx 함수를 쓰기위한 값
+    void    *win;       //mlx win 함수를 쓰기위한 void 값
+    int						width;      //화면 자체의 길이
+	int						height;     //화면 자체의 높이
+    t_img   img;            //이미지를 넣을 때 필요한 구조체
 }               t_data;
 
 
--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 // 생각해보아야할 것
 //기본적으로 argv에 .cub파일만 받아오는지
 
-int	ft_key_handling(int keycode, t_data *data)
+int	ft_key_handling(int keycode, t_data *data)  //이건 esc 컨트롤
 {
 	if (keycode == 65307)
 	{
@@ -14,7 +14,7 @@ int	ft_key_handling(int keycode, t_data *data)
 		exit(0);
 	}
 }
-int	ft_exit_handling(void *param)
+int	ft_exit_handling(void *param)   // 이건 크로스 표시 컨트롤
 {
 	t_data	*data;
 
@@ -48,11 +48,15 @@ int     main(int argc, char **argv)
 	}
     close(fd);
     */
+   //여기서 파싱을 하면 될 것 같습니다.
     data.mlx = mlx_init();
-    init_fdf_program(&data);
+    init_cub3d_program(&data);    //데이타를 초기화 합니다
     mlx_get_screen_size(data.mlx, &data.width, &data.height);
     data.win = mlx_new_window (data.mlx, data.width, data.height, "LOVE");
     data.img.img = mlx_new_image(data.mlx, data.img.width, data.img.height);
+    data.img.buffer = mlx_get_data_addr(data.img.img, &data.img.pixel_bits,
+			&data.img.line_bytes, &data.img.endian);
+    //여기에 파싱한 맵 데이터를 이미지로 찍어내는 함수를 넣어야할 듯 합니다
     mlx_key_hook(data.win, ft_key_handling, &data);
     mlx_hook(data.win, 17, 0, ft_exit_handling, &data);
     mlx_loop (data.mlx);
